af1d33cd1e3d0379b86ac651d4ba93dbe8a8ca9b
강동현 <rkdehdgus57@gmail.com>
2022-12-14 22:42:36 +0900

    2단계 - 1~4번 문제 답안

---
+++ 2step/02.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+
+if num1 >= 90:
+    print("A")
+elif 89 >= num1 >= 80:
+    print("B")
+elif 79 >= num1 >= 70:
+    print("C")
+elif 69 >= num1 >= 60:
+    print("D")
+else:
+    print("F")
